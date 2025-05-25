b5031a620338ba14afb516910d987bec918e2dee
gesifeta <adamgemechu@gmail.com>
2025-05-25 12:52:15 +0300

    added timeout

--- index.js
+++ index.js
@@ -89,37 +89,42 @@ function allowDragg(e) {
 function dropItem(e, item_id) {
   e.preventDefault();
   let item = e.dataTransfer.getData("text");
+  e.target.style.backgroundColor = "inherit";
   if (item_id === item) {
     e.target.appendChild(document.getElementById(item));
     score++;
     document.getElementById("game-score").textContent = score;
   } else {
     let temp = document.getElementById("game-score").textContent;
+    document.getElementById("game-score").style.color = "red";
     document.getElementById("game-score").textContent = "X";
     e.target.style.color = "red";
     const time = setTimeout(() => {
       document.getElementById("game-score").textContent = temp;
+      document.getElementById("game-score").style.color = "green";
     }, 500);
   }
-  console.log("score = ", score, "length = ", reshufled.length);
+  clearTimeout(time);
   if (score !== 0 && reshufled.length == score) {
     document.getElementById("game-score").textContent = "WON!";
   }
 }
 // set on drag start
 function startDragg(e) {
-  let data = e.dataTransfer.setData("text", e.target.id);
+ e.dataTransfer.setData("text", e.target.id);
   e.target.classList.add("drag-start");
 }
 // set on drag leave
 function dragLeaves(e) {
   e.preventDefault();
-   e.target.style.border = "none";
+  e.target.style.border = "1px dotted gray";
+  e.target.style.backgroundColor = "inherit";
 }
 // set on dragg enter
 function draggEnter(e) {
   e.preventDefault();
-  e.target.style.border = "2px solid green"
+  e.target.style.border = "2px dashed green";
+  e.target.style.backgroundColor = "lightgray";
 }
 document.addEventListener("DOMContentLoaded", () => {
   const collectionSoda = document.querySelector(".container-collections");
@@ -139,7 +144,6 @@ document.addEventListener("DOMContentLoaded", () => {
   for (let item in reshufled) {
     typeof item != "string" && reshufled.splice(reshufled.indexOf(item), 1);
   }
-  console.log(reshufled);
   reshufled.forEach((item, index) => {
     console.log(`item ${index} = ${item}`);
     const playerStack = document.querySelector(".container-stacks");
@@ -154,7 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
     playerStack.append(play_card);
     play_card.append(playStackImage);
     playStackImage.ondragover = (e) => allowDragg(e);
-    playStackImage.ondragleave =(e)=>dragLeaves(e)
+    playStackImage.ondragleave = (e) => dragLeaves(e);
     playStackImage.ondragenter = (e) => draggEnter(e);
     playStackImage.ondrop = (e) => dropItem(e, playStackImage.id);
   });
