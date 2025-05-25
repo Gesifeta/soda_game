b1d5fd1e5de899259113cefa734fbe87898a4f31
gesifeta <adamgemechu@gmail.com>
2025-05-25 12:42:16 +0300

    added drag events

--- index.js
+++ index.js
@@ -1,66 +1,161 @@
 export const collections = [
   {
+    id: "image-1",
     name: "coca",
     image: "assets/coca.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-2",
     name: "sprite",
     image: "assets/sprite.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
-    name: "mango",
-    image: "assets/mango.jpg",
+    id: "image-3",
+    name: "pepsi",
+    image: "assets/pepsi.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-4",
     name: "apple",
     image: "assets/apple.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-5",
     name: "orange",
     image: "assets/orange.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-6",
     name: "malt",
     image: "assets/malt.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-7",
     name: "soda",
     image: "assets/soda.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-8",
     name: "schweppes",
     image: "assets/schweppes.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-9",
     name: "thumbsup",
     image: "assets/thumbsup.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
   {
+    id: "image-10",
     name: "lemenade",
     image: "assets/lemenade.jpg",
     prize: (Math.random() * 500).toFixed(),
   },
 ];
-document.addEventListener("DOMContentLoaded",()=>{
-    const collectionSoda = document.querySelector(".container-collections")
-    collections.map((collection,index)=>{
-        let coll = document.createElement("div")
-        collectionSoda?.append(coll)
+const reshufled = [];
+function reshufle() {
+  for (let i = 0; i < collections.length; i++) {
+    let random = (Math.random() * collections.length).toFixed(0);
+    if (reshufled.find((item) => item === reshufled[Number(random)]) !== -1) {
+      reshufled[random] = collections[i];
+    }
+    while (
+      reshufled[Number(random)] !== undefined &&
+      reshufled.length <= collections.length
+    ) {
+      if (random > collections.length - 1) {
+        random = 0;
+      }
+      random = (Math.random() * collections.length).toFixed(0);
+    }
+    reshufled[Number(random)] = collections[i];
+  }
+}
+let score = 0;
+reshufle();
+// allow dragg
+function allowDragg(e) {
+  e.preventDefault();
+}
+// drop the item
+function dropItem(e, item_id) {
+  e.preventDefault();
+  let item = e.dataTransfer.getData("text");
+  if (item_id === item) {
+    e.target.appendChild(document.getElementById(item));
+    score++;
+    document.getElementById("game-score").textContent = score;
+  } else {
+    let temp = document.getElementById("game-score").textContent;
+    document.getElementById("game-score").textContent = "X";
+    e.target.style.color = "red";
+    const time = setTimeout(() => {
+      document.getElementById("game-score").textContent = temp;
+    }, 500);
+  }
+  console.log("score = ", score, "length = ", reshufled.length);
+  if (score !== 0 && reshufled.length == score) {
+    document.getElementById("game-score").textContent = "WON!";
+  }
+}
+// set on drag start
+function startDragg(e) {
+  let data = e.dataTransfer.setData("text", e.target.id);
+  e.target.classList.add("drag-start");
+}
+// set on drag leave
+function dragLeaves(e) {
+  e.preventDefault();
+   e.target.style.border = "none";
+}
+// set on dragg enter
+function draggEnter(e) {
+  e.preventDefault();
+  e.target.style.border = "2px solid green"
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const collectionSoda = document.querySelector(".container-collections");
 
-        let im = document.createElement("img")
-        im.src = collection.image
-        collectionSoda?.append(im)
-    })
-    
+  collections.map((collection, index) => {
+    let bottle = document.createElement("div");
+    collectionSoda?.append(bottle);
+    let collectionImage = document.createElement("img");
+    collectionImage.setAttribute("draggable", "true");
+    collectionImage.src = collection.image;
+    collectionImage.id = `${collection.id}`;
+    collectionSoda?.append(collectionImage);
+    collectionImage.ondragstart = (e) => startDragg(e);
+    collectionImage.ondragleave = (e) => dragLeaves(e);
+  });
 
-})
-
+  for (let item in reshufled) {
+    typeof item != "string" && reshufled.splice(reshufled.indexOf(item), 1);
+  }
+  console.log(reshufled);
+  reshufled.forEach((item, index) => {
+    console.log(`item ${index} = ${item}`);
+    const playerStack = document.querySelector(".container-stacks");
+    let play_card = document.createElement("div");
+    let playStackImage = document.createElement("div");
+    playStackImage.style.minWidth = "100px";
+    playStackImage.style.minHeight = "150px";
+    playStackImage.style.borderTopLeftRadius = "12px";
+    playStackImage.style.borderTopRightRadius = "12px";
+    playStackImage.style.border = "1px dotted gray";
+    playStackImage.id = `${item.id}`;
+    playerStack.append(play_card);
+    play_card.append(playStackImage);
+    playStackImage.ondragover = (e) => allowDragg(e);
+    playStackImage.ondragleave =(e)=>dragLeaves(e)
+    playStackImage.ondragenter = (e) => draggEnter(e);
+    playStackImage.ondrop = (e) => dropItem(e, playStackImage.id);
+  });
+});
